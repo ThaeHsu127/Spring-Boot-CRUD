3156846481d8c934fbf31728305b7424a430e4bd
Thae Hsu <thaehsu032@gmail.com>
2021-10-22 23:14:44 +0630

    task schedule

--- src/main/java/com/test/springbootcrud/UpdatePriceTask.java
+++ src/main/java/com/test/springbootcrud/UpdatePriceTask.java
@@ -1,25 +1,23 @@
 package com.test.springbootcrud;
 
-import com.test.springbootcrud.model.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@Component
 public class UpdatePriceTask {
     private static final Logger log = LoggerFactory.getLogger(UpdatePriceTask.class);
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
- @Scheduled(fixedRate=5000)//specifies the interval between method invocations, measured from the start time of each invocation
+    @Scheduled(fixedRate=5000)//specifies the interval between method invocations, measured from the start time of each invocation
     public void reportCurrentTime() {
 
-     log.info("The time is now used fixedRate{}", dateFormat.format(new Date()));
+        log.info("The time is now used fixedRate{}", dateFormat.format(new Date()));
     }
-//    private Double initialPrice = null;
+  //  private Double initialPrice = null;
 //    private double instancePrice;
 //    @Scheduled(fixedRate =60000)
 //    private double getInstancePrice(Product product){
