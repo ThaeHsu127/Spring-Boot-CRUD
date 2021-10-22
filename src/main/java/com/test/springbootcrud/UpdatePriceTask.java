38950823a5fe709c94010cc7a8210825d236af92
Thae Hsu <thaehsu032@gmail.com>
2021-10-22 22:59:51 +0630

    task schedule

--- src/main/java/com/test/springbootcrud/UpdatePriceTask.java
+++ src/main/java/com/test/springbootcrud/UpdatePriceTask.java
@@ -3,12 +3,11 @@ package com.test.springbootcrud;
 import com.test.springbootcrud.model.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 
 @Component
 public class UpdatePriceTask {
@@ -21,22 +20,22 @@ public class UpdatePriceTask {
 
      log.info("The time is now used fixedRate{}", dateFormat.format(new Date()));
     }
-    private Double initialPrice = null;
-    private double instancePrice;
-    @Scheduled(fixedRate =60000)
-    private double getInstancePrice(Product product){
-        try {
-
-            instancePrice = product.getPrice();
-            System.out.print(instancePrice);
-            if (initialPrice == null) {
-                initialPrice = instancePrice;
-            } else if (instancePrice > initialPrice) {
-                // do this
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return instancePrice;
-    }
+//    private Double initialPrice = null;
+//    private double instancePrice;
+//    @Scheduled(fixedRate =60000)
+//    private double getInstancePrice(Product product){
+//        try {
+//
+//            instancePrice = product.getPrice();
+//            System.out.print(instancePrice);
+//            if (initialPrice == null) {
+//                initialPrice = instancePrice;
+//            } else if (instancePrice > initialPrice) {
+//               log.info("Price is",instancePrice);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return instancePrice;
+//    }
 }
